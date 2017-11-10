8535288e670020ab9ebdcfe3fce3ca0bf2057614
Danny Robinson <dannyrobinson@gmx.com>
2017-11-10 09:41:27 +0000

    Got rid of unnecessary main.js, added background rainbow squares

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 var device
 var form
+var cnv
+var boxes = []
+
+function rInt(max) {return Math.floor(Math.random() * max)}
+
+class canvasPad {
+  constructor(_x, _y) {
+    this.x = _x
+    this.y = _y
+    this.color = color(rInt(255),rInt(255),rInt(255))
+    this.width = 50
+  }
+  display() {
+    fill(this.color)
+    rect(this.x, this.y, this.width, this.width)
+  }
+}
 
 window.onload = function () {
   let connectButton = document.getElementById("connectButton")
@@ -10,6 +27,20 @@ window.onload = function () {
     ev.preventDefault()
   })
 }
+
+function setup() {
+    cnv = createCanvas(windowWidth, windowHeight)
+    for (let x = 10; x < windowWidth; x += 70) {
+      for (let y = 10; y < windowHeight; y += 70){
+        let p = new canvasPad(x,y)
+        boxes.push(p)
+      }
+    }
+    for (b in boxes) {
+      boxes[b].display()
+    }
+}
+
 
 function connect() {
   device = new LaunchpadMKII
