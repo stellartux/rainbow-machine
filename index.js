faca6439673b0653c59cc62770cde6b9186cde21
Danny Robinson <dannyrobinson@gmx.com>
2018-01-14 07:57:24 +0000

    Fix

--- index.js
+++ index.js
@@ -82,13 +82,8 @@ function setup() {
 }
 
 function draw() {
-<<<<<<< HEAD
   for (b of boxes) {
     b.render()
-=======
-  for (b in boxes) {
-    boxes[b].render()
->>>>>>> 20b1194469a6fc41ef682ac0c949d16cdd94169d
   }
 }
 function windowResized() {
