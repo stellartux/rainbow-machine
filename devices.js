9be30c4993d23524982aa73f9bb61ef704b6806f
Danny Robinson <dannyrobinson@gmx.com>
2018-04-29 19:45:07 +0100

    Added random color display on the Launchpad

--- devices.js
+++ devices.js
@@ -1,6 +1,20 @@
-class GenericMIDIDevice {
+class LaunchpadMKII {
   constructor() {
-    this.connected = false
+    this.name = "Launchpad MK2" // The name of the device as it appears in the menu
+    this.manufacturer = [0, 32, 41, 2, 24] // Manufacturer's SysEx header bytes
+    this.maxColorValue = 63 // highest per-channel color value
+    this.layout = [
+      11, 12, 13, 14, 15, 16, 17, 18, 19,
+      21, 22, 23, 24, 25, 26, 27, 28, 29,
+      31, 32, 33, 34, 35, 36, 37, 38, 39,
+      41, 42, 43, 44, 45, 46, 47, 48, 49,
+      51, 52, 53, 54, 55, 56, 57, 58, 59,
+      61, 62, 63, 64, 65, 66, 67, 68, 69,
+      71, 72, 73, 74, 75, 76, 77, 78, 79,
+      81, 82, 83, 84, 85, 86, 87, 88, 89,
+      104, 105, 106, 107, 108, 109, 110, 111
+    ] //
+    this.connect()
   }
 
   connect() {
@@ -11,38 +25,18 @@ class GenericMIDIDevice {
       } else {
         this.output = WebMidi.getOutputByName(this.name)
         this.input = WebMidi.getInputByName(this.name)
-        this.input.addListener("noteon", "all", (e) => {
-          console.log(`noteon ${e.data}`)
+        /*
+        this.input.addListener("noteon", "all", e => this.noteOn(e))
+        .addListener("noteoff", "all",  e => this.noteOff(e))
+        .addListener("controlchange", "all",  e => {
+          e.data[2] === 127 ? this.noteOn(e) : this.noteOff(e)
         })
-        .addListener("noteoff", "all", (e) => {
-          console.log(`noteoff ${e.data}`)
-        })
-        .addListener("controlchange", "all", (e) => {
-          console.log(`cc ${e.data}`)
-        })
+        */
+        this.clear()
         this.test()
-        this.connected = true
       }
     }, true)
 }
-  clear() {} // Clear the display
-  stopText() {} // Stop any text
-  sendText(message, color, speed, loop) {}
-  sendCommand() {}
-  flash() {}
-  messageReceived() {}
-  noteOn() {}
-  noteOff() {}
-}
-
-class LaunchpadMKII extends GenericMIDIDevice {
-  constructor() {
-    super()
-    this.name = "Launchpad MK2" // The name of the device as it appears in the menu
-    this.manufacturer = [0, 32, 41, 2, 24] // Manufacturer's SysEx header bytes
-    this.colorDepth = 8 // per-channel color bitdepth
-    this.connect()
-  }
 
   sendCommand(cmd) {
     this.output.sendSysex(this.manufacturer, cmd)
@@ -53,28 +47,39 @@ class LaunchpadMKII extends GenericMIDIDevice {
     this.sendCommand([14,0])
   }
 
-  parseColor(color) {
-    return color
-  }
-
-  parseTextColor(color) {
-    return color
-  }
-
-  sendText(message, color=2, speed=4, loop=false) {
-    let parsedText = Array.from(message, letter => letter.charCodeAt(0))
-    color = this.parseTextColor(color)
-    this.sendCommand([20, color, (loop?1:0), speed].concat(parsedText))
-  }
-
   stopText() {
     this.sendCommand([20])
   }
 
-  test(i=3) {
+  sendText(message, color=2, speed=4, loop=false) {
+    let parsedText = Array.from(message, letter => letter.charCodeAt(0))
+    this.sendCommand([20, color, (loop?1:0), speed].concat(parsedText))
+  }
+
+  test(i=56) {
     this.sendText("|", i, 7)
   }
 
-  noteOn() {}
-  noteOff() {}
+  random() {
+    function getRandomInt(max) {
+      return Math.floor(Math.random() * Math.floor(max))
+    }
+    for (let j = 0; j < 80; j += 10) {
+      let payload = [11]
+      for (let i = 0; i < 10; i++) {
+        payload.push(this.layout[i+j])
+        payload.push(getRandomInt(63))
+        payload.push(getRandomInt(63))
+        payload.push(getRandomInt(63))
+      }
+      this.sendCommand(payload)
+    }
+  }
+
+  noteOn(e) {
+      this.sendCommand([11, e.data[1], 63, 63, 63])
+  }
+  noteOff(e) {
+      this.sendCommand([11, e.data[1], 0, 0, 0])
+  }
 }
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const device = new LaunchpadMKII
 let boxes = []
+let realBoxes = []
 let form
+let animateBackground = true
+let vegas = false
+
+function toggleBg() {
+  animateBackground = !animateBackground
+  if (animateBackground) {
+    loop()
+  } else {
+    noLoop()
+  }
+}
+
+function toggleVegas() {
+    vegas = !vegas
+    device.clear()
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // main launchpad communication functionality
@@ -45,14 +62,14 @@ class canvasPad {
     this.width = 50
     this.lerpTime = 3000
   }
-  render() {
+  render(delta) {
     if (this.elapsed > this.lerpTime) {
       this.elapsed = 0
       this.newColor()
     }
     fill(lerpColor(this.lastColor, this.nextColor, (this.elapsed / this.lerpTime)))
     rect(this.x, this.y, this.width, this.width)
-    this.elapsed += 83
+    this.elapsed += delta
   }
   newColor() {
     this.lastColor = this.nextColor
@@ -61,27 +78,67 @@ class canvasPad {
   }
 }
 
+// Equivalent code for generating color boxes for the pads on the LPMKII
+class realPad extends canvasPad {
+  constructor(a) {
+    super(0, 0)
+    this.address = a
+    this.lerpTime = 100
+  }
+
+  render(delta) {
+    if (this.elapsed > this.lerpTime) {
+      this.elapsed = 0
+      this.newColor()
+    }
+    let lc = lerpColor(this.lastColor, this.nextColor, (this.elapsed / this.lerpTime))
+    let payload = [11, this.address]
+    payload.push(floor(red(lc)/8))
+    payload.push(floor(green(lc)/8))
+    payload.push(floor(blue(lc)/8))
+    device.sendCommand(payload)
+    this.elapsed += delta
+  }
+  newColor() {
+    this.lastColor = this.nextColor
+    this.nextColor = rColor()
+    this.lerpTime = rInt(4000,125)
+  }
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight)
   frameRate(12)
   noStroke()
   makeBoxes()
+  for (let a of device.layout) {
+    let p = new realPad(a)
+    realBoxes.push(p)
+  }
 }
 
 function draw() {
+  let delta = 1000 / frameRate()
   for (b of boxes) {
-    b.render()
+    b.render(delta)
+  }
+  if (vegas) {
+    for (let r of realBoxes) {
+      r.render(delta)
+    }
   }
 }
+
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight)
   makeBoxes()
 }
+
 function makeBoxes() {
   boxes = []
   for (let x = 10; x < (windowWidth - 30); x += 70) {
     for (let y = 10; y < (windowHeight - 30); y += 70){
-      let p = new canvasPad(x,y)
+      let p = new canvasPad(x, y)
       boxes.push(p)
     }
   }
